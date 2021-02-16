8d1c07310cbb37b1c2ff0ff634d964fb52c2067d
Rustam16 <muhambetovrustam1@gmail.com>
2021-02-16 22:59:16 +0600

    finish

--- app/src/main/java/space/example/register/MainActivity.java
+++ app/src/main/java/space/example/register/MainActivity.java
@@ -3,7 +3,6 @@ package space.example.register;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -19,28 +18,26 @@ public class MainActivity extends AppCompatActivity {
     private TextView eAttemptsInfo;
     private TextView btnSinUp;
 
+    //В этом счетчике хранится количество попыток
     private int counter = 5;
+
+    //boolean переменная используемая для проверки
     boolean isValid = false;
 
-    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Связываем представления XML с элементами кода Java
         email = findViewById(R.id.email);
         pass = findViewById(R.id.pass);
         eAttemptsInfo = findViewById(R.id.tvAttempts);
         login = findViewById(R.id.login);
         btnSinUp = findViewById(R.id.btnSinUp);
 
-        sharedPreferences = getApplicationContext().getSharedPreferences("CredentialsDB", MODE_PRIVATE);
-        if (sharedPreferences !=null){
-            String savedUserEmail = sharedPreferences.getString("UserEmail","");
-            String  savedPassword = sharedPreferences.getString("Password","");
-        }
-
+        // Установил слушатель на текстовое представление SignUp
         btnSinUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,36 +45,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // onClick для кнопки и логика на нажатие кнопки входа
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String inputEmail, inputPassword;
 
-                inputEmail = email.getText().toString();
-                inputPassword = pass.getText().toString();
+                // Получение пользовательского ввода
+                String inputEmail = email.getText().toString();
+                String inputPassword = pass.getText().toString();
 
+                // Проверка  на пустоту логина
                 if (inputEmail.isEmpty() || inputPassword.isEmpty()) {
                     Toast toast = Toast.makeText(MainActivity.this, "Пожалуйста, введите все данные правильно", Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.TOP, 0, 0);
                     toast.show();
                 } else {
 
+                    //Проверяем вводимые пользователем данные
                     isValid = validate(inputEmail, inputPassword);
 
+                    /* Проверяем вводимые пользователем данные
+                    Если неверно */
                     if (!isValid) {
+                        //Decrement the counter
                         counter--;
-
-                        eAttemptsInfo.setText("Осталось попыток :" + counter);
                         Toast.makeText(MainActivity.this, "Введены неверные учетные данные!", Toast.LENGTH_SHORT).show();
+                        //показывает оставшиеся попыти
+                        eAttemptsInfo.setText("Осталось попыток :" + counter);
 
+                        //Отключить кнопку входа, если не осталось попыток
                         if (counter == 0) {
                             login.setEnabled(false);
+
+                        } else {
+                            // Показывает сообщение об ошибке
                             Toast.makeText(MainActivity.this, "Вы использовали все свои попытки, попробуйте еще раз позже!", Toast.LENGTH_LONG).show();
                         }
-
+                        //если верно
                     } else {
                         Toast.makeText(MainActivity.this, "Авторизация успешна", Toast.LENGTH_SHORT).show();
-
                         Intent intent = new Intent(MainActivity.this, HomePageActivity.class);
                         startActivity(intent);
                     }
--- app/src/main/java/space/example/register/Register.java
+++ app/src/main/java/space/example/register/Register.java
@@ -11,54 +11,57 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Map;
 import java.util.jar.Attributes;
 
 public class Register extends AppCompatActivity {
 
+    /* Определяем элементы пользовательского интерфейса */
     EditText reg_email, reg_pass;
     Button btn_reg;
     TextView login_btn;
 
+    /* Создаем объект класса Credentials */
     public static Credentials credentials;
 
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor sharedPreferencesEditor ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
 
+        /* Связываем элементы пользовательского интерфейса с элементами макета XML */
         reg_email = findViewById(R.id.reg_email);
         reg_pass = findViewById(R.id.reg_pass);
 
         btn_reg = findViewById(R.id.btn_reg);
         login_btn = findViewById(R.id.login_btn);
 
-        sharedPreferences = getApplicationContext().getSharedPreferences("CredentialsDB", MODE_PRIVATE);
-        sharedPreferencesEditor = sharedPreferences.edit();
-
-
+        // A listener for click events on the Register Button
         btn_reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // Получаю данные из полей
                 String registerEmail = reg_email.getText().toString();
                 String registerPassword = reg_pass.getText().toString();
 
+                // Проверяем правильность полей
                 if (validate(registerEmail, registerPassword)) {
+                    // Добавляем учетные данные в нашу базу данных
                     credentials = new Credentials(registerEmail, registerPassword);
-                    Toast.makeText(Register.this, "Регистрация прошла успешно!", Toast.LENGTH_SHORT).show();
-
-                    sharedPreferencesEditor.putString("UserEmil", registerEmail);
-                    sharedPreferencesEditor.putString("RegisterPassword", registerPassword);
 
                     startActivity(new Intent(Register.this, MainActivity.class));
+                    Toast.makeText(Register.this, "Регистрация прошла успешно!", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
     }
 
+    //Функция для проверки вводимых учетных данных от пользователя
     boolean validate(String email, String password) {
+        //Проверяем, пусто ли имя и длина поля пароля 8 символов
         if (email.isEmpty() || (password.length() < 8)) {
             Toast.makeText(this, "Пожалуйста, введите все данные, пароль должен содержать не менее 8 символов!", Toast.LENGTH_SHORT).show();
             return false;
@@ -66,7 +69,8 @@ public class Register extends AppCompatActivity {
         return true;
     }
 
+    //onClick переход в MainActivity если у вас уже есть аккаунт
     public void login_btn(View view) {
-        startActivity( new Intent(Register.this, MainActivity.class));
+        startActivity(new Intent(Register.this, MainActivity.class));
     }
 }
